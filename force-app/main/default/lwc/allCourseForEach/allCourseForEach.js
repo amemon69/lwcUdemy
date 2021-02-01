bce55c09bc2e9908ee0d62a30339280e5a75bc98
amemon69 <mail4aamirmemon@gmail.com>
2021-01-31 18:52:51 -0800

    Foreach

---
+++ force-app/main/default/lwc/allCourseForEach/allCourseForEach.js
@@ -0,0 +1,10 @@
+import { LightningElement } from 'lwc';
+
+export default class AllCourseForEach extends LightningElement {
+    allCourseDetail =[
+        {courseName:'LWC', courseDuration:'30 days'},
+        {courseName:'SOQL', courseDuration:'15 days'},
+        {courseName:'APEX', courseDuration:'25 days'},
+        {courseName:'VF', courseDuration:'20 days'}
+    ]
+}
